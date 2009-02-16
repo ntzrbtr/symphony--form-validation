eccbfd56f44fca264a943623a6f333ffd1216f31
Thomas Off <thomas@Erde.(none)>
2009-02-16 16:37:47 +0100

    Add preferences settings for entering the section field name where form validation rulesets are stored at.

--- extension.driver.php
+++ extension.driver.php
@@ -3,13 +3,13 @@
 /**
  * Include external library for form validation.
  */
-@require_once(EXTENSIONS . 'formvalidation/lib/validation.php');
+@require_once(EXTENSIONS . '/formvalidation/lib/validation.php');
 
 /**
  * Form validation extension class
  *
  * @package formvalidation
- * @author Thomas Off
+ * @author Thomas Off - retiolum.de <info@retiolum.de>
  **/
 class extension_formvalidation extends Extension {
 
@@ -17,7 +17,6 @@ class extension_formvalidation extends Extension {
 	 * Set array containing all the data for the 'About' page in the extension administration
 	 *
 	 * @return void
-	 * @author Thomas Off
 	 **/
 	public function about() {
 		return array(
@@ -37,7 +36,6 @@ class extension_formvalidation extends Extension {
 	 * Return an array containing the delegates that this extension subscribes itself to.
 	 *
 	 * @return array
-	 * @author Thomas Off
 	 **/
 	public function getSubscribedDelegates() {
 		return array(
@@ -65,7 +63,12 @@ class extension_formvalidation extends Extension {
 				'page' 		=> '/frontend/',
 				'delegate' 	=> 'EventPreSaveFilter',
 				'callback' 	=> 'processEventData',
-			),						
+			),
+			array(
+				'page' 		=> '/system/preferences/',
+				'delegate' 	=> 'AddCustomPreferenceFieldsets',
+				'callback' 	=> 'appendPreferences',
+			),
 		);
 	}
 	
@@ -74,7 +77,6 @@ class extension_formvalidation extends Extension {
 	 *
 	 * @param array $context
 	 * @return void
-	 * @author Thomas Off
 	 **/
 	public function addFilterToEventEditor($context) {
 		$context['options'][] = array(
@@ -89,7 +91,6 @@ class extension_formvalidation extends Extension {
 	 *
 	 * @param array $context
 	 * @return array
-	 * @author Thomas Off
 	 **/
 	public function addFilterDocumentationToEvent($context) {
 		if (!in_array('formvalidation', $context['selected'])) {
@@ -105,7 +106,6 @@ class extension_formvalidation extends Extension {
 	 *
 	 * @param array $context
 	 * @return array
-	 * @author Thomas Off
 	 **/
 	public function processEventData($context) {
 		// Check whether something should be done at all.
@@ -137,6 +137,70 @@ class extension_formvalidation extends Extension {
 			$result,
 			(!$result ? 'Errors detected in the form.' : NULL),
 		);
-		
 	}	
+	
+	/**
+	 * Add a new preferences field for the name of the section where form valiation rulesets are stored.
+	 *
+	 * @param array $context
+	 * @return void
+	 */
+	public function appendPreferences($context) {
+		// Initialize the group.
+		$group = new XMLElement('fieldset');
+		$group->setAttribute('class', 'settings');
+		$group->appendChild(new XMLElement('legend', 'Form Validation'));
+
+		// Add the field for the section name.
+		$label = Widget::Label('Section Name');
+		$label->appendChild(Widget::Input('settings[formvalidation][section]', General::Sanitize($this->getSection())));		
+		$group->appendChild($label);
+		
+		// Add the field for the field name.
+		$label = Widget::Label('Field Name');
+		$label->appendChild(Widget::Input('settings[formvalidation][field]', General::Sanitize($this->getField())));		
+		$group->appendChild($label);
+		
+		// Append everything to the wrapper.
+		$context['wrapper']->appendChild($group);
+	}
+
+	/**
+	 * Get the preference setting for the section name where form validation rulesets are stored in.
+	 *
+	 * @return string
+	 */
+	public function getSection(){
+		if (class_exists('ConfigurationAccessor')) {
+			return ConfigurationAccessor::get('section', 'formvalidation');
+		}	
+		return $this->_Parent->Configuration->get('section', 'formvalidation');
+	}
+
+	/**
+	 * Get the preference setting for the name of the field inside the section where form validation rulesets are stored in.
+	 *
+	 * @return string
+	 */
+	public function getField(){
+		if (class_exists('ConfigurationAccessor')) {
+			return ConfigurationAccessor::get('field', 'formvalidation');
+		}	
+		return $this->_Parent->Configuration->get('field', 'formvalidation');
+	}
+	
+	/**
+	 * Do some uninstall work by removing the set preferences.
+	 *
+	 * @return void
+	 */
+	public function uninstall() {
+		if (class_exists('ConfigurationAccessor')) {
+			ConfigurationAccessor::remove('formvalidation');
+		}
+		else {
+			$this->_Parent->Configuration->remove('formvalidation');
+		}		
+		$this->_Parent->saveConfig();
+	}
 }
